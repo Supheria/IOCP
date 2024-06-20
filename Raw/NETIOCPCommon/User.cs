5bf7ff062b178f88c10cb85c4b138f6158d91256
Supheria <supheria@gmail.com>
2024-06-20 21:07:04 +0800

    add raw

---
+++ Raw/NETIOCPCommon/User.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Net
+{
+    public class User
+    {
+        public string Userid
+        { get; set; }
+        public string Password
+        { get; set; }
+        public string Username
+        { get; set; }
+    }
+}
